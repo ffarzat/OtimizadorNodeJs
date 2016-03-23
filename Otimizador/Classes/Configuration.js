667d817e7f9231d84a2d426e596a92dece2575bc
ffarzat <fabiofarzat@gmail.com>
2016-03-23 20:30:22 -0300

    Adding IHeuristic, GA and Configuration classes [refs #1]

---
+++ Otimizador/Classes/Configuration.js
@@ -0,0 +1,7 @@
+//Represents a Configuration File
+var Configuration = (function () {
+    function Configuration() {
+    }
+    return Configuration;
+})();
+//# sourceMappingURL=Configuration.js.map
---
+++ Otimizador/Classes/Heuristics/GA.ts
@@ -0,0 +1,7 @@
+﻿//Implemetation of a Genetic Improvement
+class GA implements IHeuristc {
+
+    public Setup(configuration: Configuration): boolean {
+        return false;
+    } 
+}
